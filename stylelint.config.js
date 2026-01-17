849ab26834b9339942b89f5850617e5b284d5d24
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-01-17 21:10:56 +0500

    chore: update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,5 @@
 export default {
   extends: ['stylelint-config-clean-order', 'stylelint-config-standard'],
   rules: { 'selector-class-pattern': null },
+  ignoreFiles: ['node_modules/**', 'coverage/**', 'dist/**'],
 };
