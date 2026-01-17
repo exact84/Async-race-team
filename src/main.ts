6fffca12594b113c7f241d75573278aa9eada602
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-01-18 02:26:18 +0500

    chore: cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,7 @@ import 'modern-normalize/modern-normalize.css';
 
 import './styles/styles.css';
 import { App } from './app/app';
-import { serviceProvider } from './services/service-provider';
 
 const app = new App();
 
 app.initialize();
-
-const winnersService = serviceProvider.winnersService();
-
-const winners = await winnersService.getAll();
-
-console.warn(winners);
