6e184700189c819ac892bf5ae81865293b00468a
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-01-19 04:08:14 +0500

    chore: cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,8 @@
 import 'modern-normalize/modern-normalize.css';
 
+import { App } from './app/app';
 import './styles/styles.css';
 
-import { button } from '@ripetchor/dom';
+const app = new App();
 
-import { TrackController } from './features/track/track.controller';
-import { TrackView } from './features/track/track.view';
-import { serviceProvider } from './services/service-provider';
-
-const engineService = serviceProvider.engineService();
-const garageService = serviceProvider.garageService();
-const winnersService = serviceProvider.winnersService();
-
-const trackController = new TrackController(
-  new TrackView(),
-  engineService,
-  garageService,
-  winnersService
-);
-
-await trackController.initialize();
-
-const startRaceButton = button(
-  {
-    click: () => {
-      trackController.startRace().catch(console.warn);
-    },
-  },
-  'Start race'
-);
-
-const stopRaceButton = button(
-  {
-    click: () => {
-      trackController.stopRace().catch(console.warn);
-    },
-  },
-  'Stop race'
-);
-
-document.body.append(startRaceButton, stopRaceButton, trackController.getView());
+app.initialize();
