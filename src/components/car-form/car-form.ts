7a173adf20b0877b2de305684fa6fa45f150da61
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-01-21 19:49:02 +0500

    feat: implement car form

---
+++ src/components/car-form/car-form.ts
@@ -0,0 +1,94 @@
+import { form, input, label } from '@ripetchor/dom';
+
+import type { Car } from '../../services/garage-service/types';
+
+import { createRandomCar } from '../../services/garage-service/utilities';
+import { Component, defineElement } from '../../shared/component/component';
+import { Button } from '../button/button';
+import { CarImage } from '../car-image/car-image';
+import styles from './car-form.module.css';
+
+export type CarFormProperties = CreateFormProperties | UpdateFormProperties;
+
+interface CreateFormProperties {
+  mode: 'create';
+  onSubmit: FormSubmitCallback;
+}
+
+type FormSubmitCallback = (car: Car) => void;
+
+type UpdateFormProperties = Car & { mode: 'update'; onSubmit: FormSubmitCallback };
+
+export class CarForm extends Component<CarFormProperties> {
+  private readonly buttonSubmit = new Button({ textContent: 'Submit' });
+
+  private readonly carImage = new CarImage({ color: 'white', size: 'lg' });
+
+  private readonly inputColor = input({
+    change: (event) => {
+      this.carImage.setColor(event.currentTarget.value);
+    },
+    id: 'car-color',
+    type: 'color',
+  });
+
+  private readonly inputName = input({
+    autofocus: true,
+    className: styles.input,
+    id: 'car-name',
+    type: 'text',
+  });
+
+  private readonly labelColor = label(
+    { className: styles.label, htmlFor: 'car-color' },
+    'Color',
+    this.inputColor
+  );
+
+  private readonly labelName = label(
+    { className: styles.label, htmlFor: 'car-name' },
+    'Name',
+    this.inputName
+  );
+
+  private readonly formElement = form(
+    {
+      className: styles.form,
+      submit: (event) => {
+        event.preventDefault();
+
+        const id = this.props.mode === 'update' ? this.props.id : Number.NaN;
+
+        this.props.onSubmit({ color: this.inputColor.value, id, name: this.inputName.value });
+      },
+    },
+    this.labelName,
+    this.labelColor,
+    this.carImage,
+    this.buttonSubmit
+  );
+
+  public constructor(properties: CarFormProperties) {
+    super(properties);
+
+    if (properties.mode === 'create') {
+      const { color, name } = createRandomCar();
+
+      this.carImage.setColor(color);
+      this.inputColor.value = color;
+      this.inputName.value = name;
+    }
+
+    if (properties.mode === 'update') {
+      this.carImage.setColor(properties.color);
+      this.inputColor.value = properties.color;
+      this.inputName.value = properties.name;
+    }
+  }
+
+  public render(): DocumentFragment | HTMLElement {
+    return this.formElement;
+  }
+}
+
+defineElement('car-from', CarForm);
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import 'modern-normalize/modern-normalize.css';
 
 import { App } from './app/app';
 import './styles/styles.css';
+import { CarForm } from './components/car-form/car-form';
 
 const app = new App();
 
 app.initialize();
+
+const carForm = new CarForm({
+  mode: 'create',
+  onSubmit: (data): void => {
+    console.warn(data);
+  },
+});
+
+document.body.prepend(carForm);
