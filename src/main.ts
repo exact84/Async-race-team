c83efd4ef8f748b7ad99e4f58d8d1fbe4cc79008
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-01-21 23:47:20 +0500

    chore: cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,7 @@ import 'modern-normalize/modern-normalize.css';
 
 import { App } from './app/app';
 import './styles/styles.css';
-import { CarForm } from './components/car-form/car-form';
 
 const app = new App();
 
 app.initialize();
-
-const carForm = new CarForm({
-  mode: 'create',
-  onSubmit: (data): void => {
-    console.warn(data);
-  },
-});
-
-document.body.prepend(carForm);
