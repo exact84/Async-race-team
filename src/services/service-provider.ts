643dea44d8575d564f664e152c760f577639696b
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-01-23 02:01:28 +0500

    chore: cleanup

--- src/services/service-provider.ts
+++ src/services/service-provider.ts
@@ -1,7 +1,6 @@
 import { httpClient } from '../shared/http-client/http-client';
 import { EngineService } from './engine-service/engine.service';
 import { GarageService } from './garage-service/garage.service';
-import { RaceService } from './race-service/race.service';
 import { WinnersService } from './winners-service/winners.service';
 
 export const serviceProvider = {
@@ -10,13 +9,6 @@ export const serviceProvider = {
   },
   garageService(): GarageService {
     return GarageService.getInstance(httpClient);
-  },
-  raceService(): RaceService {
-    return RaceService.getInstance(
-      serviceProvider.garageService(),
-      serviceProvider.engineService(),
-      serviceProvider.winnersService()
-    );
   },
   winnersService(): WinnersService {
     return WinnersService.getInstance(httpClient, serviceProvider.garageService());
