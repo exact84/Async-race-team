e0ff2edc9fdb36587dfe4eac726c2211d040e682
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-01-24 00:34:37 +0500

    feat: save theme to local storage

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,8 @@ import { Navbar } from '../components/navbar/navbar';
 import { ThemeSwitcher } from '../components/theme-switcher/theme-switcher';
 import { ToastContainer } from '../components/toast/toast';
 import { toastService } from '../components/toast/toast.service';
+import { getFromLocalStorage, setToLocalStorage } from '../shared/local-storage';
+import { isAppTheme } from '../shared/type-guards';
 import styles from './app.module.css';
 import { ROUTES } from './routes';
 
@@ -17,23 +19,26 @@ export class App {
   public initialize(): void {
     this.router.initialize().then(
       (outlet) => {
+        const storedTheme = getFromLocalStorage('app-theme', isAppTheme, 'system');
+        document.documentElement.dataset.theme = storedTheme;
+
         const navbar = new Navbar({ links: NAV_LINKS });
 
         const themeSwitcher = new ThemeSwitcher({
-          onChange: (v): void => {
-            document.documentElement.dataset.theme = v;
+          onChange: (theme): void => {
+            document.documentElement.dataset.theme = theme;
+            setToLocalStorage('app-theme', theme);
           },
-          selectedTheme: 'dark',
+          selectedTheme: storedTheme,
         });
 
         const appHeader = header({ className: styles.header }, navbar, themeSwitcher);
 
+        const toastContainer = new ToastContainer();
+
         this.root.append(appHeader, outlet);
 
-        document.body.append(this.root);
-
-        const toastContainer = new ToastContainer();
-        document.body.append(toastContainer);
+        document.body.append(this.root, toastContainer);
       },
       () => {
         toastService.show({ message: 'App initialization failed', type: 'error' });
