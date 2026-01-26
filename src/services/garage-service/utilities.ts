d561458392e0524be4d6b9f5e3e97c61f8a0385b
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-01-26 21:47:08 +0500

    refactor: update random car generation

--- src/services/garage-service/utilities.ts
+++ src/services/garage-service/utilities.ts
@@ -1,11 +1,11 @@
 import type { Car } from './types';
 
 import { getRandomHexColor, getRandomItem } from '../../shared/utilities';
-import { CAR_BRANDS, CAR_MODELS } from './constants';
+import { CARS } from './constants';
 
 export function createRandomCar(): Omit<Car, 'id'> {
-  const brand = getRandomItem(CAR_BRANDS);
-  const model = getRandomItem(CAR_MODELS);
+  const { brand, models } = getRandomItem(CARS);
+  const model = getRandomItem(models);
   const color = getRandomHexColor();
 
   return { color, name: `${brand} ${model}` };
