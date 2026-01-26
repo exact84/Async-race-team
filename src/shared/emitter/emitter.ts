ec739c4f0ac8d7706294166cb15fac8c61a34452
ripetchor <115036520+ripetchor@users.noreply.github.com>
2026-01-27 00:39:47 +0500

    Minor fixes (#68)

--- src/shared/emitter/emitter.ts
+++ src/shared/emitter/emitter.ts
@@ -1,16 +1,15 @@
 type EventHandler<T> = (...payload: T[]) => void;
 
 export class Emitter<EventsMap extends object> {
-  private readonly listeneres: Partial<{
-    [K in keyof EventsMap]: Set<EventHandler<EventsMap[K]>>;
-  }> = {};
+  private readonly listeners: Partial<{ [K in keyof EventsMap]: Set<EventHandler<EventsMap[K]>> }> =
+    {};
 
   public clear<Key extends keyof EventsMap>(event?: Key): void {
     if (event) {
-      Reflect.deleteProperty(this.listeneres, event);
+      Reflect.deleteProperty(this.listeners, event);
     } else {
-      for (const key in this.listeneres) {
-        Reflect.deleteProperty(this.listeneres, key);
+      for (const key in this.listeners) {
+        Reflect.deleteProperty(this.listeners, key);
       }
     }
   }
@@ -19,7 +18,7 @@ export class Emitter<EventsMap extends object> {
     event: Key,
     ...payload: EventsMap[Key] extends never ? [] : [EventsMap[Key]]
   ): void {
-    const set = this.listeneres[event];
+    const set = this.listeners[event];
 
     if (!set) {
       return;
@@ -34,14 +33,14 @@ export class Emitter<EventsMap extends object> {
     event: Key,
     handler: EventHandler<EventsMap[Key]>
   ): VoidFunction {
-    if (!this.listeneres[event]) {
-      this.listeneres[event] = new Set();
+    if (!this.listeners[event]) {
+      this.listeners[event] = new Set();
     }
 
-    this.listeneres[event].add(handler);
+    this.listeners[event].add(handler);
 
     return () => {
-      const set = this.listeneres[event];
+      const set = this.listeners[event];
 
       if (!set) {
         return;
@@ -50,7 +49,7 @@ export class Emitter<EventsMap extends object> {
       set.delete(handler);
 
       if (set.size === 0) {
-        Reflect.deleteProperty(this.listeneres, event);
+        Reflect.deleteProperty(this.listeners, event);
       }
     };
   }
